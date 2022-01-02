9400b70cb95c3733ce992e3a2b35898a5ed031cc
Bryan Guevara <bryanguevara@Bryans-MacBook-Air.local>
2022-01-01 16:38:18 -0800

    Added Backspace button

--- interface.js
+++ interface.js
@@ -2,7 +2,8 @@
 let HTML_keypad = document.querySelector("#keypad");
 let keys = [..."789/456x123-.0=+"];
 let screen_lines = document.querySelectorAll(".screen-line");
-let clear_button = document.querySelector("#clear-button");
+let clear_button = document.querySelectorAll(".option-button")[0];
+let backspace_button = document.querySelectorAll(".option-button")[1];
 
 
 //------------- Objects ---------------------------
@@ -10,6 +11,7 @@ let exp = {
     nums: new Array(2),
     current: 0,
     op: "",
+    error: false,
     clear(){
         this.nums = [undefined, undefined];
         this.op = "";
@@ -19,13 +21,38 @@ let exp = {
 }
 //--------------------------------------------------
 
-
+function calculate(){
+    if((exp.op === "/") && exp.nums[1] === "0"){
+        exp.clear();
+        exp.error = true;
+        screen_lines[0].value = "";
+        screen_lines[1].value = "ERROR";
+        return;
+    }
+    screen_lines[0].value = screen_lines[1].value;
+    let result = operate(exp.op, parseFloat(exp.nums[0]), parseFloat(exp.nums[1]));
+    exp.clear();
+    exp.nums[0] = result;
+    screen_lines[1].value = `${Math.round(exp.nums[0] * 1000)/1000}`;
+}
 
 clear_button.addEventListener("click", ()=> {
     screen_lines[0].value = "";
     screen_lines[1].value = "";
     screen_lines[1].placeholder = "0";
     exp.clear();
+    exp.error = false;
+});
+
+backspace_button.addEventListener("click", ()=>{
+    if(screen_lines[1].value.length === 0) return;
+    if(exp.error) return;
+    screen_lines[1].value = screen_lines[1].value.substring(0, screen_lines[1].value.length - 1);
+    if(exp.op === ""){ exp.nums[0] = exp.nums[0].substring(0, exp.nums[0].length - 1); }
+    else if (exp.op !== "" && exp.nums[1] === undefined){ exp.op = ""; }
+    else {
+        exp.nums[1] = exp.nums[1].substring(0, exp.nums[1].length - 1);
+    }
 });
 
 keys.forEach(key => {
@@ -35,7 +62,7 @@ keys.forEach(key => {
 
     if(HTML_key.textContent.match(/\d/)){
         HTML_key.addEventListener("click", ()=> {
-            if(screen_lines[1].value === "ERROR") return;
+            if(exp.error) return;
             if(exp.nums[exp.current] === undefined){
                 exp.nums[exp.current] = HTML_key.textContent;
             } else {
@@ -48,7 +75,7 @@ keys.forEach(key => {
         switch(HTML_key.textContent){
             case ".":
                 HTML_key.addEventListener("click", ()=>{
-                    if(screen_lines[1].value === "ERROR") return;
+                    if(exp.error) return;
                     if(exp.nums[exp.current] === undefined){
                         screen_lines[1].value += "0.";
                         exp.nums[exp.current] = "0.";
@@ -63,23 +90,15 @@ keys.forEach(key => {
             case "/":
             case "x":
                 HTML_key.addEventListener("click", ()=>{
-                    if(screen_lines[1].value === "ERROR") return;
+                    if(exp.error) return;
                     if(screen_lines[1].value.length === 0) return;
                     if(exp.current === 1 && exp.nums[1] !== undefined){
-                        if((exp.op === "/") && exp.nums[1] === "0"){
-                            exp.clear();
-                            screen_lines[0].value = "";
-                            screen_lines[1].value = "ERROR";
-                            return;
+                        calculate();
+                        if(!exp.error){
+                            exp.current = 1;
+                            exp.op = HTML_key.textContent;
+                            screen_lines[1].value += exp.op;
                         }
-
-                        screen_lines[0].value = screen_lines[1].value;
-                        let result = operate(exp.op, parseFloat(exp.nums[0]), parseFloat(exp.nums[1]));
-                        exp.clear();
-                        exp.nums[exp.current] = result;
-                        exp.current = 1;
-                        exp.op = HTML_key.textContent;
-                        screen_lines[1].value = `${Math.round(exp.nums[0] * 1000)/1000}${exp.op}`;
                     }
                     else {
                         if(exp.op !== "") return;
@@ -93,17 +112,7 @@ keys.forEach(key => {
             case "=":
                 HTML_key.addEventListener("click", ()=>{
                     if(exp.nums[0] === undefined || exp.nums[1] === undefined) return;
-                    if((exp.op === "/") && exp.nums[1] === "0"){
-                        exp.clear();
-                        screen_lines[0].value = "";
-                        screen_lines[1].value = "ERROR";
-                        return;
-                    }
-                    screen_lines[0].value = screen_lines[1].value;
-                    let result = operate(exp.op, parseFloat(exp.nums[0]), parseFloat(exp.nums[1]));
-                    exp.clear();
-                    exp.nums[0] = result;
-                    screen_lines[1].value = `${Math.round(exp.nums[0] * 1000)/1000}`;
+                    calculate();
                 });
                 break;
         }
